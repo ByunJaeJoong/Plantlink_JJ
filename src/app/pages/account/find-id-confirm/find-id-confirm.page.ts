310934e5d429391458d1e0ec75c325469680293e
yuri <yrjange@kncuration.com>
2022-07-14 16:33:12 +0900

    아이디찾기 확인 페이지 추가

--- src/app/pages/account/find-id-confirm/find-id-confirm.page.ts
+++ src/app/pages/account/find-id-confirm/find-id-confirm.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-find-id-confirm',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./find-id-confirm.page.scss'],
 })
 export class FindIdConfirmPage implements OnInit {
+  constructor(private navController: NavController) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  //아이디찾기
+  goLoign() {
+    this.navController.navigateForward(['/login']);
   }
 
+  //비번찾기
+  findPw() {
+    this.navController.navigateForward(['/find-password']);
+  }
 }
