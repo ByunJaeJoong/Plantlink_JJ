e29a7f8e6f8036bba68b07db633c2d42c7fa8848
yuri <yrjange@kncuration.com>
2022-07-20 16:33:33 +0900

    식물도감 페이지 추가

--- src/app/pages/plant-book/plant-book.module.ts
+++ src/app/pages/plant-book/plant-book.module.ts
@@ -7,14 +7,10 @@ import { IonicModule } from '@ionic/angular';
 import { PlantBookPageRoutingModule } from './plant-book-routing.module';
 
 import { PlantBookPage } from './plant-book.page';
+import { SharedModule } from '../shared/fake-tab/shared.moduls';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    PlantBookPageRoutingModule
-  ],
-  declarations: [PlantBookPage]
+  imports: [CommonModule, FormsModule, IonicModule, PlantBookPageRoutingModule, SharedModule],
+  declarations: [PlantBookPage],
 })
 export class PlantBookPageModule {}
