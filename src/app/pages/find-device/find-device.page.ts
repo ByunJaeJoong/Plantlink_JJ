eb9df5a7a701a735c908ed6db4b1a6d02f9aff1c
yuri <yrjange@kncuration.com>
2022-07-21 14:29:01 +0900

    블루투스 끝

--- src/app/pages/find-device/find-device.page.ts
+++ src/app/pages/find-device/find-device.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { AlertService } from 'src/app/services/alert.service';
 
 @Component({
   selector: 'app-find-device',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./find-device.page.scss'],
 })
 export class FindDevicePage implements OnInit {
+  constructor(private navController: NavController, private alertService: AlertService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  //홈화면으로 가기
+  goHome() {
+    this.navController.navigateForward(['/connect-device']);
   }
 
+  //장치 없을 때 뜨는 alert
+  goSearch() {
+    this.alertService.cancelOkBtn('two-btn', '1개의 장치를 연결했습니다:)<br>식물의 종류를 선택하시겠어요?', '', '취소', '확인').then(ok => {
+      if (ok) {
+        this.navController.navigateRoot(['/plant-search']);
+      }
+    });
+  }
 }
