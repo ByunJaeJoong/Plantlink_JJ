3c8603eeeb1a52b0ee858e18a755eeae39d0ce4d
jeongin <jijeon@kncuration.com>
2022-08-08 14:31:03 +0900

    diray-write page 수정중

--- src/app/pages/diary-write/diary-write.page.ts
+++ src/app/pages/diary-write/diary-write.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalController, NavController } from '@ionic/angular';
+import { ModalController, NavController, NavParams } from '@ionic/angular';
+import { AlertService } from 'src/app/services/alert.service';
+import { CommonService } from 'src/app/services/common.service';
+import { DbService } from 'src/app/services/db.service';
 import { DiaryWriteCameraPage } from '../diary-write-camera/diary-write-camera.page';
 
 @Component({
@@ -8,9 +11,36 @@ import { DiaryWriteCameraPage } from '../diary-write-camera/diary-write-camera.p
   styleUrls: ['./diary-write.page.scss'],
 })
 export class DiaryWritePage implements OnInit {
-  constructor(private modalController: ModalController, private navController: NavController) {}
+  diary: any = {
+    diaryId: '',
+    dateCreated: '',
+    content: '',
+    images: [],
+    selectDate: '',
+  };
+
+  constructor(
+    private modalController: ModalController,
+    private navController: NavController,
+    private navParams: NavParams,
+    private db: DbService,
+    private common: CommonService,
+    private alert: AlertService
+  ) {
+    this.diary.selectDate = this.navParams.get('selectDate');
+  }
 
   ngOnInit() {}
+
+  // 일기장 등록
+  async save() {
+    this.diary.diaryId = this.common.generateFilename();
+    this.diary.dateCreated = new Date().toISOString();
+
+    await this.db.updateAt(`diary`, this.diary);
+    this.alert.okBtn('', '일기장 등록이 완료되었습니다.');
+    this.close();
+  }
 
   //모달 닫기
   close() {
