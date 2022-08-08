74b96f90ccfccc04de9f652863d5e7e902036720
jeongin <jijeon@kncuration.com>
2022-08-09 08:13:03 +0900

    달력 클릭시 이벤트 수정중

--- src/app/pages/diary/diary.page.ts
+++ src/app/pages/diary/diary.page.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { ModalController, NavController } from '@ionic/angular';
 import { localeKo, MbscCalendarEvent, MbscEventcalendarOptions } from '@mobiscroll/angular';
+import { Observable } from 'rxjs';
+import { first, map } from 'rxjs/operators';
+import { DbService } from 'src/app/services/db.service';
 import { DiaryWritePage } from '../diary-write/diary-write.page';
 
 @Component({
@@ -9,10 +13,16 @@ import { DiaryWritePage } from '../diary-write/diary-write.page';
   styleUrls: ['./diary.page.scss'],
 })
 export class DiaryPage implements OnInit {
+  userId: string = localStorage.getItem('userId');
   date: Date = new Date();
   selectDate: string = '';
+  diaryData$: Observable<any>;
 
   myEvents: MbscCalendarEvent[] = [];
+  diaryDetail: any;
+  diaryData: any;
+  a: any = [];
+  b: any = [];
 
   eventSettings: MbscEventcalendarOptions = {
     locale: localeKo,
@@ -37,14 +47,41 @@ export class DiaryPage implements OnInit {
       let date = new Date(args.date + '');
       date.setHours(date.getHours() + 9);
       this.selectDate = date.toISOString();
+      // this.diaryDetail = this.diaryData$.pipe(map(data: any)=> {})
+      // this.diaryData = this.diaryData$.
+      this.diaryDetail = this.diaryData.filter(data => data.selectDate?.indexOf(this.selectDate) > -1);
+      console.log(this.diaryDetail);
     },
   };
-  constructor(private navController: NavController, private modalController: ModalController) {
+  constructor(private http: HttpClient, private navController: NavController, private modalController: ModalController, private db: DbService) {
     // diary 페이지에 오면 당일에 표시
     this.selectDate = new Date().toISOString();
+    this.getData();
   }
 
-  ngOnInit() {}
+  async ngOnInit() {
+    this.a = await this.db.collection$(`diary`).pipe(first()).toPromise();
+    console.log(this.a);
+  }
+
+  async getData() {
+    const defaultColor: string = '#ff6d42';
+
+    this.diaryData$ = this.db.collection$(`diary`).pipe(
+      map(diarys => {
+        diarys.map((diary: any) => {
+          (diary.color = defaultColor),
+            (diary.end = diary.selectDate),
+            (diary.id = diary.diaryId),
+            (diary.start = diary.selectDate),
+            (diary.title = diary.content),
+            (diary.image = diary.images[0]);
+        });
+        return diarys;
+      })
+    );
+    this.diaryData = await this.diaryData$.pipe(first()).toPromise();
+  }
 
   test(ev: any) {
     this.date = ev.month;
