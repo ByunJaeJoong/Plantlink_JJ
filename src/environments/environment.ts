36e3bf9ffc583e44698a22251db1d172e8a1b6b4
Jungsik <jskim@kncuration.com>
2022-08-08 12:33:54 +0900

    개발 시작!

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,15 @@
 
 export const environment = {
   production: false,
-  firebase: {},
+  firebaseConfig: {
+    apiKey: 'AIzaSyDVyB75vQ6kwgkwHIPOlf6V-UGycktB8B0',
+    authDomain: 'plantlink-beb65.firebaseapp.com',
+    projectId: 'plantlink-beb65',
+    storageBucket: 'plantlink-beb65.appspot.com',
+    messagingSenderId: '1017905341794',
+    appId: '1:1017905341794:web:1cc290f0c99b1fbdec783b',
+    measurementId: 'G-V9CT41SCMB',
+  },
 };
 
 /*
