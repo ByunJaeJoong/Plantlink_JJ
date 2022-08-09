0b7630d7301bdc806ada32c0ef62d7bbc3e04582
jeongin <jijeon@kncuration.com>
2022-08-09 14:13:50 +0900

    마이페이지 정보 수정중

--- src/app/pages/info/info.page.ts
+++ src/app/pages/info/info.page.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { first } from 'rxjs/operators';
 import { AlertService } from 'src/app/services/alert.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { DbService } from 'src/app/services/db.service';
 
 @Component({
   selector: 'app-info',
@@ -8,17 +12,27 @@ import { AlertService } from 'src/app/services/alert.service';
   styleUrls: ['./info.page.scss'],
 })
 export class InfoPage implements OnInit {
-  constructor(private alertService: AlertService, private navController: NavController) {}
+  master: any = [];
+
+  constructor(private alertService: AlertService, private navController: NavController, private db: DbService, private auth: AuthService) {
+    this.getData();
+  }
 
   ngOnInit() {}
 
+  async getData() {
+    this.master = await this.db.collection$(`master`).pipe(first()).toPromise();
+  }
+
   //로그아웃 alert
-  goLogout() {
-    this.alertService.cancelOkBtn('two-btn', '정말 로그아웃 하시겠어요?', '', '취소', '확인').then(ok => {
-      if (ok) {
-        this.navController.navigateRoot(['/login-join']);
-      }
-    });
+  async goLogout() {
+    const ok = await this.alertService.cancelOkBtn('two-btn', '정말 로그아웃 하시겠어요?', '', '취소', '확인');
+
+    if (ok) {
+      await this.auth.logoutUser();
+      localStorage.removeItem('userId');
+      this.navController.navigateRoot(['/login-join']);
+    }
   }
 
   //식물 현재 상태
