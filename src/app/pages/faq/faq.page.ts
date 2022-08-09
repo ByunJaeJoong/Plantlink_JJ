932678f9ea58ecdbb0daef9fd9146ef489350e96
jeongin <jijeon@kncuration.com>
2022-08-09 13:36:50 +0900

    마이페이지 FAQ 페이지 완료

--- src/app/pages/faq/faq.page.ts
+++ src/app/pages/faq/faq.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { DbService } from 'src/app/services/db.service';
 
 @Component({
   selector: 'app-faq',
@@ -7,9 +9,17 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./faq.page.scss'],
 })
 export class FaqPage implements OnInit {
-  constructor(private navController: NavController) {}
+  faqs$: Observable<any>;
+
+  constructor(private navController: NavController, private db: DbService) {
+    this.getData();
+  }
 
   ngOnInit() {}
+
+  getData() {
+    this.faqs$ = this.db.collection$(`faq`, ref => ref.orderBy('dateCreated', 'desc'));
+  }
 
   goSetting() {
     this.navController.navigateForward(['/setting']);
