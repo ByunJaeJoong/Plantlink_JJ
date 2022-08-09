dbb1108c66fcd94779be2636ead6cf54b0aba96c
jeongin <jijeon@kncuration.com>
2022-08-09 14:13:07 +0900

    마이페이지 사용자 계약 완료

--- src/app/pages/contract/contract.page.ts
+++ src/app/pages/contract/contract.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { DbService } from 'src/app/services/db.service';
 
 @Component({
   selector: 'app-contract',
@@ -7,9 +9,17 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./contract.page.scss'],
 })
 export class ContractPage implements OnInit {
-  constructor(private navController: NavController) {}
+  master$: Observable<any>;
+
+  constructor(private navController: NavController, private db: DbService) {
+    this.getData();
+  }
 
   ngOnInit() {}
+
+  getData() {
+    this.master$ = this.db.collection$(`master`);
+  }
 
   //설정화면으로
   goSetting() {
