c4f510012acd166a344ed984e3e56a08c3106f4d
yuri <yrjange@kncuration.com>
2022-08-10 16:48:26 +0900

    주소 창 변경

--- src/app/pages/account/join-address/join-address.page.ts
+++ src/app/pages/account/join-address/join-address.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { postcode } from 'src/assets/js/postcode.js';
 
 @Component({
   selector: 'app-join-address',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./join-address.page.scss'],
 })
 export class JoinAddressPage implements OnInit {
+  @ViewChild('address_pop', { read: ElementRef, static: true }) popup!: ElementRef;
+  search: string = '';
+  store = {
+    address: '',
+  };
+  constructor(private renderer: Renderer2) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  openDaumPopup() {
+    setTimeout(() => {
+      // this.keyboard.hide();
+      this.getAddress().then(data => {
+        console.log('data', data);
+        this.search = `${data.sido} ${data.sigungu} ${data.bname}`;
+      });
+    }, 1000);
   }
 
+  getAddress(): Promise<any> {
+    return new Promise<any>((resolve, reject) => {
+      postcode(this.renderer, this.popup.nativeElement, data => {
+        resolve(data);
+      });
+    });
+  }
+
+  // 닫기
+  closeAddressPopup() {
+    this.renderer.setStyle(this.popup.nativeElement, 'display', 'none');
+  }
 }
--- src/app/pages/account/join/join.page.ts
+++ src/app/pages/account/join/join.page.ts
@@ -1,7 +1,6 @@
 import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { AlertService } from 'src/app/services/alert.service';
-import { postcode } from 'src/assets/js/postcode.js';
 
 @Component({
   selector: 'app-join',
@@ -9,37 +8,9 @@ import { postcode } from 'src/assets/js/postcode.js';
   styleUrls: ['./join.page.scss'],
 })
 export class JoinPage implements OnInit {
-  @ViewChild('address_pop', { read: ElementRef, static: true }) popup!: ElementRef;
-  search: string = '';
-  store = {
-    address: '',
-  };
-  constructor(private navController: NavController, private alertService: AlertService, private renderer: Renderer2) {}
+  constructor(private navController: NavController, private alertService: AlertService) {}
 
   ngOnInit() {}
-
-  openDaumPopup() {
-    setTimeout(() => {
-      // this.keyboard.hide();
-      this.getAddress().then(data => {
-        console.log('data', data);
-        this.search = `${data.sido} ${data.sigungu} ${data.bname}`;
-      });
-    }, 1000);
-  }
-
-  getAddress(): Promise<any> {
-    return new Promise<any>((resolve, reject) => {
-      postcode(this.renderer, this.popup.nativeElement, data => {
-        resolve(data);
-      });
-    });
-  }
-
-  // 닫기
-  closeAddressPopup() {
-    this.renderer.setStyle(this.popup.nativeElement, 'display', 'none');
-  }
 
   //아이디중복확인 1.사용가능
   checkIdOk() {
