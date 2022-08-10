6756dcb945ed963067cced52c81ca12f0210c5c2
yuri <yrjange@kncuration.com>
2022-08-10 15:53:25 +0900

    회원가입 alert 추가

--- src/app/pages/account/join/join.page.ts
+++ src/app/pages/account/join/join.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { AlertService } from 'src/app/services/alert.service';
 
 @Component({
   selector: 'app-join',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./join.page.scss'],
 })
 export class JoinPage implements OnInit {
+  constructor(private navController: NavController, private alertService: AlertService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  //아이디중복확인 1.사용가능
+  checkIdOk() {
+    this.alertService.okBtn('alert', '사용할 수 있는 아이디 입니다.', '');
   }
 
+  //아이디중복확인 1.사용불가능
+  checkIdNo() {
+    this.alertService.okBtn('alert', '사용할 수 없는 아이디 입니다.', '');
+  }
+
+  //이메일중복확인 1.사용가능
+  checkEmailOk() {
+    this.alertService.okBtn('alert', '사용할 수 있는 이메일 입니다.', '');
+  }
+
+  //아이디중복확인 1.사용불가능
+  checkEmailNo() {
+    this.alertService.okBtn('alert', '사용할 수 없는 이메일 입니다.', '');
+  }
+
+  //인증번호발송
+  checkNumber() {
+    this.alertService.okBtn('alert', '인증번호가 발송되었습니다.', '');
+  }
+
+  //인증번호 확인
+  checkedNumber() {
+    this.alertService.okBtn('alert', '인증번호가 확인되었습니다.', '');
+  }
 }
