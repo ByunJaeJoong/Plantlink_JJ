12f0c2aa26194c544eed6591c0e00b5609e68c12
jeongin <jijeon@kncuration.com>
2022-08-18 16:00:25 +0900

    블루투스 연결 완료(데이터 가져오기 수정 중)

--- src/app/pages/connect-device/connect-device.page.ts
+++ src/app/pages/connect-device/connect-device.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { BLE } from '@ionic-native/ble/ngx';
+import { BluetoothSerial } from '@ionic-native/bluetooth-serial/ngx';
 import { NavController } from '@ionic/angular';
 
 @Component({
@@ -9,8 +11,9 @@ import { NavController } from '@ionic/angular';
 })
 export class ConnectDevicePage implements OnInit {
   deviceList: any = [];
+  isConnect: boolean = false;
 
-  constructor(private navController: NavController, private route: ActivatedRoute) {
+  constructor(private navController: NavController, private route: ActivatedRoute, private bluetoothSerial: BluetoothSerial, private ble: BLE) {
     this.route.queryParams.subscribe(data => {
       for (let ob in data) {
         this.deviceList.push(JSON.parse(data[ob]));
@@ -19,6 +22,28 @@ export class ConnectDevicePage implements OnInit {
   }
 
   ngOnInit() {}
+
+  connect(id: string) {
+    try {
+      this.ble.connect(id).subscribe(data => {
+        this.read(data);
+        this.isConnect = true;
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async read(data: any) {
+    try {
+      console.log(data);
+
+      // const arduinoData = await this.ble.read(id, );
+      // console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   //홈화면으로 가기
   goHome() {
