ef7b3986d3ab4f93808921e6eff39d097a4cd4da
jeongin <jijeon@kncuration.com>
2022-08-19 12:44:32 +0900

    블루투스 연결 후 데이터 수정중

--- src/app/pages/connect-device/connect-device.page.ts
+++ src/app/pages/connect-device/connect-device.page.ts
@@ -12,6 +12,7 @@ import { NavController } from '@ionic/angular';
 export class ConnectDevicePage implements OnInit {
   deviceList: any = [];
   isConnect: boolean = false;
+  arduinoData: any = [];
 
   constructor(private navController: NavController, private route: ActivatedRoute, private bluetoothSerial: BluetoothSerial, private ble: BLE) {
     this.route.queryParams.subscribe(data => {
@@ -23,10 +24,13 @@ export class ConnectDevicePage implements OnInit {
 
   ngOnInit() {}
 
+  // 블루투스 장치를 클릭하여 그 장치와 연결시킴
   connect(id: string) {
     try {
       this.ble.connect(id).subscribe(data => {
+        // 연결시킨 장치 안에 데이터를 보냄
         this.read(data);
+        // 연결됨을 표시하기 위한 불리언 값
         this.isConnect = true;
       });
     } catch (error) {
@@ -36,13 +40,35 @@ export class ConnectDevicePage implements OnInit {
 
   async read(data: any) {
     try {
-      console.log(data);
+      console.log('클릭한 device data', data);
+      // 연결된 장치 고유 아이디
+      const deviceId = data.id;
+      // 안에서 읽어낼 서비스, 특징 아이디
+      const deviceData = data.characteristics;
 
-      // const arduinoData = await this.ble.read(id, );
-      // console.log(data);
+      deviceData.forEach((device: any) => {
+        let serviceId = device.service;
+        let characteristicId = device.characteristic;
+        let readDevice = device.properties;
+
+        readDevice.forEach(async (data: any) => {
+          if (data == 'Read') {
+            const readData = await this.ble.read(deviceId, serviceId, characteristicId);
+            console.log(readData, String.fromCharCode.apply(null, new Uint8Array(readData)));
+
+            this.arduinoData.push(readData);
+          }
+        });
+      });
+
+      this.bufferData();
     } catch (error) {
       console.log(error);
     }
+  }
+
+  bufferData() {
+    console.log(this.arduinoData, typeof this.arduinoData);
   }
 
   //홈화면으로 가기
