aaa1c956bd29d11bafacdb405cef47faf4ef3345
Jungsik <jskim@kncuration.com>
2022-08-22 14:02:06 +0900

    ios build

--- src/app/pages/device-list/device-list.page.ts
+++ src/app/pages/device-list/device-list.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BLE } from '@ionic-native/ble/ngx';
 import { NavParams, ModalController } from '@ionic/angular';
-
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 @Component({
   selector: 'app-device-list',
   templateUrl: './device-list.page.html',
