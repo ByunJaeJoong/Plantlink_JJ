e95db1d4744b3ca6dfd7560882f87c940f4cbcb9
Jungsik <jskim@kncuration.com>
2022-09-05 08:01:20 +0900

    채팅 수정

--- src/app/pages/chatting-detail/chatting-detail.page.ts
+++ src/app/pages/chatting-detail/chatting-detail.page.ts
@@ -3,7 +3,10 @@ import { ActivatedRoute } from '@angular/router';
 import { IonContent, NavController } from '@ionic/angular';
 import { Observable } from 'rxjs';
 import { first, map } from 'rxjs/operators';
+import { Chats } from 'src/app/models/chat.model';
+import { AlertService } from 'src/app/services/alert.service';
 import { ChatService } from 'src/app/services/chat.service';
+import { CommonService } from 'src/app/services/common.service';
 import { DbService } from 'src/app/services/db.service';
 
 @Component({
@@ -19,7 +22,27 @@ export class ChattingDetailPage implements OnInit {
   botId: any;
   chat$: Observable<any>;
   message: any;
-  constructor(private navController: NavController, private route: ActivatedRoute, private db: DbService, private cs: ChatService) {
+  chat: any;
+  chats: Chats = {
+    bluetoothId: '',
+    chatGroup: [],
+    chatId: '',
+    count: 0,
+    createdAt: 0,
+    deleteSwitch: false,
+    exitSwitch: false,
+    messages: [],
+    plantId: '',
+    userId: '',
+  };
+  constructor(
+    private navController: NavController,
+    private route: ActivatedRoute,
+    private db: DbService,
+    private cs: ChatService,
+    private alertService: AlertService,
+    private common: CommonService
+  ) {
     this.chatId = this.route.snapshot.queryParams.chatId;
     this.botId = this.route.snapshot.queryParams.botId;
     this.getData();
@@ -41,6 +64,25 @@ export class ChattingDetailPage implements OnInit {
         return item;
       })
     );
+  }
+  exit() {
+    this.alertService.cancelOkBtn('two-btn', '채팅 내용이 삭제됩니다.<br>채팅방을 나가시겠습니까?', '', '취소', '확인').then(ok => {
+      if (ok) {
+        this.db.updateAt(`chats/${this.chatId}`, {
+          deleteSwitch: true,
+        });
+        this.createChat();
+        this.navController.navigateRoot(['/tabs/home']);
+      }
+    });
+  }
+  async createChat() {
+    this.chats.chatId = this.common.generateFilename();
+    this.chats.createdAt = Date.now();
+    this.chats.chatGroup = [this.myId, this.botId];
+    this.chats.messages = [{ chatContent: '모든게 다 잘될거야!', createdAt: Date.now(), uid: this.botId }];
+    this.chats.userId = this.myId;
+    this.db.updateAt(`chats/${this.chats.chatId}`, this.chats);
   }
 
   sendMessage() {
