5e4fa39b541ef212c84a9e2216a57c8c0ede3bae
Jungsik <jskim@kncuration.com>
2022-09-05 12:22:29 +0900

    식물 현재 상태

--- src/app/pages/plant-detail/plant-detail.page.ts
+++ src/app/pages/plant-detail/plant-detail.page.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AlertController, NavController } from '@ionic/angular';
 import * as firebase from 'firebase';
-import { Observable } from 'rxjs';
-import { first } from 'rxjs/operators';
+import { combineLatest, Observable, of } from 'rxjs';
+import { first, switchMap } from 'rxjs/operators';
 import { DbService, docJoin, docListJoin, leftJoinDocument } from 'src/app/services/db.service';
 
 @Component({
@@ -13,13 +13,16 @@ import { DbService, docJoin, docListJoin, leftJoinDocument } from 'src/app/servi
 })
 export class PlantDetailPage implements OnInit {
   myPlantId: any;
+  userId: any;
   plantInfo$: Observable<any>;
   plant$: Observable<any>;
   name: string;
   plant: any;
   plantInfo: any;
+  userInfo$: Observable<any>;
   constructor(private navController: NavController, private route: ActivatedRoute, private db: DbService, private alertController: AlertController) {
     this.myPlantId = this.route.snapshot.queryParams.plantId;
+    this.userId = localStorage.getItem('userId');
   }
   slideOpts = {
     initialSlide: 0,
@@ -35,24 +38,47 @@ export class PlantDetailPage implements OnInit {
   }
 
   async getData() {
-    this.plantInfo$ = await this.db.doc$(`myPlant/${this.myPlantId}`).pipe(docJoin(this.db.afs, 'plantBookId', 'plantBook'));
-    this.plantInfo = await this.plantInfo$.pipe(first()).toPromise();
-    console.log(this.plantInfo);
+    if (this.myPlantId) {
+      this.plantInfo$ = await this.db
+        .collection$(`myPlant`, ref => ref.where('myPlantId', '==', this.myPlantId))
+        .pipe(leftJoinDocument(this.db.afs, 'plantBookId', 'plantBook'));
+      this.plantInfo = await this.plantInfo$.pipe(first()).toPromise();
+    } else {
+      this.userInfo$ = await this.db.doc$(`users/${this.userId}`);
+      this.plantInfo$ = this.userInfo$.pipe(
+        switchMap(user => {
+          let reads$ = [];
+          console.log(user);
+
+          user.myPlant.forEach(id => {
+            console.log(id);
+
+            const doc$ = this.db.doc$(`myPlant/${id}`).pipe(docJoin(this.db.afs, 'plantBookId', 'plantBook'));
+            reads$.push(doc$);
+          });
+          if (reads$.length > 0) {
+            return combineLatest(reads$);
+          } else {
+            return of([]);
+          }
+        })
+      );
+      this.plantInfo = await this.plantInfo$.pipe(first()).toPromise();
+    }
   }
 
-  async changeName() {
+  async changeName(myPlantId, name) {
     const alert = await this.alertController.create({
       cssClass: 'alert',
       message: '변경할 이름을 입력해주세요',
-      inputs: [{ name: 'name', type: 'text', value: `${this.plantInfo.name}` }],
+      inputs: [{ name: 'name', type: 'text', value: `${name}` }],
       buttons: [
         {
           text: '확인',
           handler: data => {
-            this.db.updateAt(`myPlant/${this.myPlantId}`, {
+            this.db.updateAt(`myPlant/${myPlantId}`, {
               name: data.name,
             });
-            this.plantInfo.name = data.name;
           },
         },
       ],
