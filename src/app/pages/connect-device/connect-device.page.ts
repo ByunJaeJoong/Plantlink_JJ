f37ebe02326820e2f11eb2aa0cabda8ddc2fb258
jeongin <jijeon@kncuration.com>
2022-09-07 16:15:15 +0900

    장치 추가 수정

--- src/app/pages/connect-device/connect-device.page.ts
+++ src/app/pages/connect-device/connect-device.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { Observable } from 'rxjs';
+import { first } from 'rxjs/operators';
+import { AlertService } from 'src/app/services/alert.service';
 import { DbService } from 'src/app/services/db.service';
 
 @Component({
@@ -10,14 +12,24 @@ import { DbService } from 'src/app/services/db.service';
 })
 export class ConnectDevicePage implements OnInit {
   bluetooth$: Observable<any>;
-  constructor(private navController: NavController, private db: DbService) {
+  myPlant: any;
+  userId: string = localStorage.getItem('userId');
+
+  constructor(private navController: NavController, private db: DbService, private alertService: AlertService) {
     this.getData();
   }
 
   ngOnInit() {}
 
-  getData() {
-    this.bluetooth$ = this.db.collection$(`bluetooth`, (ref: any) => ref.where('deleteSwitch', '==', false));
+  async getData() {
+    this.bluetooth$ = this.db.collection$(`bluetooth`, (ref: any) => ref.where('userId', '==', this.userId).where('deleteSwitch', '==', false));
+
+    this.myPlant = await this.db
+      .collection$(`myPlant`, (ref: any) =>
+        ref.where('userId', '==', this.userId).where('deleteSwitch', '==', false).where('cancelSwitch', '==', false)
+      )
+      .pipe(first())
+      .toPromise();
   }
   //홈화면으로 가기
   goHome() {
@@ -26,6 +38,20 @@ export class ConnectDevicePage implements OnInit {
 
   //장치찾기로
   findDevice() {
-    this.navController.navigateForward(['/find-device']);
+    if (this.myPlant.length == 0) {
+      this.emptyAlert();
+    } else {
+      this.navController.navigateForward(['/find-device']);
+    }
+  }
+
+  emptyAlert() {
+    this.alertService
+      .cancelOkBtn('two-btn', '현재 등록된 식물이 없습니다.<br>식물 추가를 통해 식물을 등록하시겠어요?', '', '취소', '확인')
+      .then(ok => {
+        if (ok) {
+          this.navController.navigateForward(['/plant-book']);
+        }
+      });
   }
 }
